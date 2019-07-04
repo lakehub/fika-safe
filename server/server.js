33bd896cedffeb7efc80fc8e4d6790a6152e43d2
samuelomondi22 <omondisam696@gmail.com>
2019-07-04 14:20:37 +0300

    conflict

--- server/server.js
+++ server/server.js
@@ -3,14 +3,7 @@ import sourceMapSupport from 'source-map-support'
 import { MongoClient } from 'mongodb';
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
-<<<<<<< HEAD
-import { Sacco, Rider }from "./db.models";
-require("babel-polyfill");
-=======
 require('babel-polyfill');
-
-
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
 
 // UNIQUE VALIDATOR
 const mongooseUniqueValidator = require('mongoose-unique-validator');
@@ -44,11 +37,6 @@ var processQuery = qpm({
 app.use(bodyParser.json());
 
 // mongoose models
-<<<<<<< HEAD
-//import { Sacco, Rider } from 'db.models.js'
-=======
-import { Sacco, Rider } from './db.models.js'
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
 // OUR SERVER CODE WILL GO HEREa
 
 app.get('/', (req, res) => {
@@ -58,29 +46,10 @@ app.get('/', (req, res) => {
 
 let db = null;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
 mongoose.connect('mongodb://127.0.0.1:27017/fika-safe', { useNewUrlParser: true })
     .then(async () => {
         app.listen(3000, () => {
             console.log("Listening on port 3000")
         });
     });
-<<<<<<< HEAD
-
-// mongoose.connect('mongodb://localhost/fika-safe').then((client) => {
-//     db = connect.db('fika-safe');
-
-//     // START THE SERVER
-//     app.listen(3000, () => {
-//         console.log("Listening on port 3001")
-//     });
-// }).catch((err) => {
-//     console.log(err.stack);
-
-// });
-=======
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
 
