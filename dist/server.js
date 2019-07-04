47845aa04936f15b741da5e9acd6c233cc652694
samuelomondi22 <omondisam696@gmail.com>
2019-07-04 14:39:16 +0300

    cinflict2

--- dist/server.js
+++ dist/server.js
@@ -6,12 +6,6 @@ var _mongodb = require("mongodb");
 
 var _mongoose = _interopRequireWildcard(require("mongoose"));
 
-<<<<<<< HEAD
-var _db = require("./db.models");
-=======
-var _dbModels = require("./db.models.js");
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
-
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -20,11 +14,7 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-<<<<<<< HEAD
-require("babel-polyfill"); // UNIQUE VALIDATOR
-=======
 require('babel-polyfill'); // UNIQUE VALIDATOR
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
 
 
 var mongooseUniqueValidator = require('mongoose-unique-validator');
@@ -56,13 +46,8 @@ var processQuery = qpm({
   }
 });
 app.use(bodyParser.json()); // mongoose models
-<<<<<<< HEAD
-//import { Sacco, Rider } from 'db.models.js'
 // OUR SERVER CODE WILL GO HEREa
-=======
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
 
-// OUR SERVER CODE WILL GO HEREa
 app.get('/', function (req, res) {
   res.json("this is our first server page");
 });
@@ -89,17 +74,5 @@ regeneratorRuntime.mark(function _callee() {
       }
     }
   }, _callee);
-<<<<<<< HEAD
-}))); // mongoose.connect('mongodb://localhost/fika-safe').then((client) => {
-//     db = connect.db('fika-safe');
-//     // START THE SERVER
-//     app.listen(3000, () => {
-//         console.log("Listening on port 3001")
-//     });
-// }).catch((err) => {
-//     console.log(err.stack);
-// });
-=======
 })));
->>>>>>> 9a4b41cf95b2dea9339a13b3338b614e0166fe29
 //# sourceMappingURL=server.js.map
