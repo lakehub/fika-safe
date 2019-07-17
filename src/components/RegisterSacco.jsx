37ffe9a8a6709b7491fcff2866db2f05e0642ed8
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-07-17 15:31:02 +0300

    validate date and email

--- src/components/RegisterSacco.jsx
+++ src/components/RegisterSacco.jsx
@@ -5,21 +5,150 @@ import { makeStyles } from '@material-ui/core/styles';
 class RegisterSacco extends Component {
   constructor(props) {
     super(props);
-    const x = Date.now();
-    const defaultDate = new Date(x).toLocaleDateString('en-US');
-    console.log(defaultDate);
+
     this.state = {
-      date: ''
+      dataField: {
+        name: '',
+        email: '',
+        telephone_number: '',
+        description: '',
+        website: '',
+        address: '',
+        registration_number: '',
+        status: '',
+        date_founded: '',
+        membership: '',
+        username: '',
+        pwd1: '',
+        pwd2: ''
+      },
+      // error fields
+      errMessage: {
+        nameErr: '',
+        emailErr: '',
+        telephone_numberErr: '',
+        descriptionErr: '',
+        websiteErr: '',
+        addressErr: '',
+        registration_numberErr: '',
+        statusErr: '',
+        date_foundedErr: '',
+        membershipErr: '',
+        usernameErr: '',
+        passwordErr: ''
+      }
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.createSacco = this.createSacco.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  onChangeDate(e) {
-    const date = e.target.value;
+  // using deep destructuring
+  handleChange({ target: { value, name } }) {
     this.setState({
-      date
+      dataField: { [name]: value }
     });
+  }
+
+  // this is the  validating function
+  validate = () => {
+    const {
+      dataField: { email, date_founded }
+    } = this.state;
+
+    // const nameErr = '';
+    let emailErr = '';
+    // const telephone_numberErr = '';
+    // const descriptionErr = '';
+    // const websiteErr = '';
+    // const addressErr = '';
+    // const registrationErr = '';
+    // const statusErr = '';
+    let date_foundedErr = '';
+    // const membershipErr = '';
+    // const usernameErr = '';
+    // const passwordErr = '';
+
+    // date validation
+    const isValidDate = Date.parse(date_founded);
+    if (isNaN(isValidDate)) {
+      // when is not valid date logic
+      date_foundedErr = 'invalid date';
+      this.setState({ errMessage: { date_foundedErr } });
+      // return false;
+    }
+    console.log(date_foundedErr);
+
+    // email validation
+    if (!email.includes('@')) {
+      emailErr = 'invalid email';
+    }
+    console.log(emailErr);
+    if (emailErr) {
+      this.setState({ errMessage: { emailErr } });
+      return false;
+    }
+
+    return true;
+  };
+
+  // SaccoAdd
+  handleSubmit(event) {
+    event.preventDefault();
+    // const form = document.forms.saccoAdd;
+
+    const {
+      dataField: {
+        name,
+        email,
+        telephone_number,
+        description,
+        website,
+        address,
+        registration_number,
+        status,
+        date_founded,
+        membership,
+        username,
+        password2
+      }
+      // errMessage: {
+      //   nameErr,
+      //   emailErr,
+      //   telephone_numberErr,
+      //   descriptionErr,
+      //   websiteErr,
+      //   addressErr,
+      //   registration_numberErr,
+      //   statusErr,
+      //   date_foundedErr,
+      //   membershipErr,
+      //   usernameErr,
+      //   passwordErr
+      // }
+    } = this.state;
+
+    const isValid = this.validate();
+    if (isValid) {
+      this.createSacco({
+        contacts: {
+          email,
+          telephone_number
+        },
+        about: {
+          description,
+          website
+        },
+        name,
+        address,
+        registration_number,
+        status,
+        date_founded,
+        membership,
+        username,
+        password: password2
+      });
+    }
   }
 
   createSacco(newSacco) {
@@ -33,41 +162,41 @@ class RegisterSacco extends Component {
       .catch(err => console.log(err));
   }
 
-  // SaccoAdd
-  handleSubmit(event) {
-    event.preventDefault();
-    const form = document.forms.saccoAdd;
-    this.createSacco({
-      contacts: {
-        email: form.email.value,
-        telephone_number: form.telephone_number.value
-      },
-      about: {
-        // description: form.description.value,
-        website: form.website.value
-      },
-      name: form.name.value,
-      address: form.address.value,
-      registration_number: form.registration_number.value,
-      status: form.status.value,
-      date_founded: form.date_founded.value,
-      membership: form.membership.value
-    });
-
-    form.name.value = '';
-    form.email.value = '';
-    form.telephone_number.value = '';
-    // form.description.value = '';
-    form.website.value = '';
-    form.address.value = '';
-    form.registration_number.value = '';
-    form.status.value = '';
-    form.date_founded.value = '';
-    form.membership.value = '';
-  }
-
   render() {
-    const { date } = this.state;
+    const {
+      errMessage: {
+        nameErr,
+        emailErr,
+        telephone_numberErr,
+        descriptionErr,
+        websiteErr,
+        addressErr,
+        registration_numberErr,
+        statusErr,
+        date_foundedErr,
+        membershipErr,
+        usernameErr,
+        passwordErr
+      }
+    } = this.state;
+
+    const {
+      dataField: {
+        name,
+        email,
+        telephone_number,
+        description,
+        website,
+        address,
+        registration_number,
+        status,
+        date_founded,
+        membership,
+        username,
+        pwd1,
+        pwd2
+      }
+    } = this.state;
 
     // const classes = useStyles();
     return (
@@ -126,28 +255,33 @@ class RegisterSacco extends Component {
                         </label>
                         <input
                           type="text"
+                          value={name}
+                          onChange={this.handleChange}
                           className="form-control"
                           name="name"
-                          id="first_name"
-                          placeholder="first name"
+                          placeholder="name"
                           title="enter your first name if any."
                         />
+                        <div>{nameErr}</div>
                       </div>
                     </div>
 
                     <div className="form-group">
                       <div className="col-xs-6">
-                        <label htmlFor="phone">
+                        <label htmlFor="registration_number">
                           <h4>Registration Number</h4>
                         </label>
                         <input
                           type="text"
+                          value={registration_number}
+                          onChange={this.handleChange}
                           className="form-control"
                           name="registration_number"
                           id="registration"
                           placeholder="enter registration number"
                           title="enter your phone number if any."
                         />
+                        <div>{registration_numberErr}</div>
                       </div>
                     </div>
 
@@ -161,9 +295,12 @@ class RegisterSacco extends Component {
                           className="form-control"
                           name="address"
                           id="address"
+                          value={address}
+                          onChange={this.handleChange}
                           placeholder="address"
                           title="enter your adress"
                         />
+                        <div>{addressErr}</div>
                       </div>
                     </div>
                     <div className="form-group">
@@ -174,11 +311,14 @@ class RegisterSacco extends Component {
                         <input
                           type="text"
                           className="form-control"
+                          value={membership}
+                          onChange={this.handleChange}
                           id="membership"
                           name="membership"
                           placeholder="membership"
                           title="enter your membership"
                         />
+                        <div>{membershipErr}</div>
                       </div>
                     </div>
                     <div className="form-group">
@@ -189,11 +329,14 @@ class RegisterSacco extends Component {
                         <input
                           name="status"
                           type="text"
+                          value={status}
+                          onChange={this.handleChange}
                           className="form-control"
                           id="status"
                           placeholder="status"
                           title="status"
                         />
+                        <div>{statusErr}</div>
                       </div>
                     </div>
                     <div className="form-group">
@@ -205,10 +348,12 @@ class RegisterSacco extends Component {
                           name="email"
                           type="text"
                           className="form-control"
-                          id="email"
+                          value={email}
+                          onChange={this.handleChange}
                           placeholder="email"
                           title="enter your email"
                         />
+                        <div style={{ color: 'red' }}>{emailErr}</div>
                       </div>
                     </div>
                     <div className="form-group">
@@ -217,34 +362,34 @@ class RegisterSacco extends Component {
                           <h4>Phone</h4>
                         </label>
                         <input
+                          name="telephone_number"
                           type="text"
+                          onChange={this.handleChange}
+                          value={telephone_number}
                           className="form-control"
                           id="phone"
                           placeholder="phone"
                           title="enter Phone Number"
                         />
+                        <div>{telephone_numberErr}</div>
                       </div>
                     </div>
 
                     <div className="form-group">
                       <div className="col-xs-6">
-                        <label htmlFor="mobile">
-                          <h4>Year Founded</h4>
+                        <label htmlFor="password">
+                          <h4>Username</h4>
                         </label>
                         <input
-                          // <TextField
-                          id="date"
-                          // value={dateGte}
-                          onChange={this.defaultDate}
-                          label="End"
-                          type="date"
-                          defaultValue={new Date().toLocaleDateString('en-US')}
-                          // className={classes.textField}
-                          inputlabelprops={{
-                            shrink: true
-                          }}
-                          // />
+                          type="text"
+                          value={username}
+                          onChange={this.handleChange}
+                          className="form-control"
+                          name="username"
+                          placeholder="Description"
+                          title="username"
                         />
+                        <div>{usernameErr}</div>
                       </div>
                     </div>
 
@@ -255,11 +400,14 @@ class RegisterSacco extends Component {
                         </label>
                         <input
                           type="password"
+                          name="pwd1"
+                          value={pwd1}
+                          onChange={this.handleChange}
                           className="form-control"
-                          name="password"
                           placeholder="password"
                           title="enter password"
                         />
+                        {passwordErr}
                       </div>
                       <div className="form-group">
                         <div className="col-xs-6">
@@ -268,11 +416,14 @@ class RegisterSacco extends Component {
                           </label>
                           <input
                             type="password"
+                            name="pwd2"
+                            value={pwd2}
+                            onChange={this.handleChange}
                             className="form-control"
-                            name="password"
                             placeholder="password"
                             title="enter password"
                           />
+                          <div>{passwordErr}</div>
                         </div>
                       </div>
 
@@ -282,12 +433,15 @@ class RegisterSacco extends Component {
                             <h4>Wesite Link</h4>
                           </label>
                           <input
+                            value={website}
+                            onChange={this.handleChange}
                             type="text"
                             className="form-control"
                             name="website"
                             placeholder="eg www.nduthisacco.org.co.ke"
                             title="enter password"
                           />
+                          <div>{websiteErr}</div>
                         </div>
                       </div>
 
@@ -298,11 +452,37 @@ class RegisterSacco extends Component {
                           </label>
                           <input
                             type="text"
+                            value={description}
+                            onChange={this.handleChange}
                             className="form-control"
                             name="description"
                             placeholder="Description"
                             title="enter password"
                           />
+                          <div>{descriptionErr}</div>
+                        </div>
+                      </div>
+
+                      <div className="form-group">
+                        <div className="col-xs-6">
+                          <label htmlFor="mobile">
+                            <h4>Year Founded</h4>
+                          </label>
+                          <input
+                            // <TextField
+                            name="date_founded"
+                            value={date_founded}
+                            onChange={this.handleChange}
+                            // label="End"
+                            type="date"
+                            // defaultValue={date}
+                            // className={classes.textField}
+                            inputlabelprops={{
+                              shrink: true
+                            }}
+                            // />
+                          />
+                          <div>{date_foundedErr}</div>
                         </div>
                       </div>
                     </div>
